e66dddbf63b59950674122c5fbe581dd8e0a5612
schuttsm <stephen.schutt@gmail.com>
2018-02-21 09:41:56 +0100

    setup selenium tests on saucelab to acquire the authorization code.  use that to get a refresh token & run tests.js with mocha

--- tests.js
+++ tests.js
@@ -1,22 +1,47 @@
 require('dotenv').load();
+
+var chai = require('chai');
+var fs = require('fs');
 var FreshBooks = require('./freshbooks.js');
-var assert = require('chai').assert;
+var request = require('request');
 
-describe('FreshBooks Client', function() {
+describe('fresbooks', function() {
+  this.timeout(1000 * 20);
+
+  var token;
   var freshbooks;
-  before(function() {
-    var access_token = process.env.FBOOKS_ACCESS_TOKEN;
-    var refresh_token = process.env.FBOOKS_REFRESH_TOKEN;
-    if (!access_token || !refresh_token)
-      throw new Error('Required environment variables FBOOKS_ACCESS_TOKEN and/or FBOOKS_REFRESH_TOKEN were not populated');
-
-    freshbooks = new FreshBooks(access_token, refresh_token);
+  it('should get an access token correctly', function(done) {
+    var code = fs.readFileSync('code.txt').toString();
+    request({
+      method: 'POST',
+      url: process.env.token_url,
+      headers: {
+        'content-type': 'application/json',
+        'Api-Version': 'alpha'
+      },
+      json: {
+        grant_type: 'authorization_code',
+        client_secret: process.env.client_secret,
+        code: code,
+        client_id: process.env.client_id,
+        redirect_uri: 'https://localhost:8081/fbooks-callback'
+      }
+    }, function(err, response, body) {
+      if (!body.refresh_token) {
+        done(new Error(body.error + ' ' + JSON.stringify(body)));
+      }
+      else {
+        freshbooks = new FreshBooks(body.access_token, body.refresh_token);
+        done();
+      }
+    });
   });
 
   it('should get the current user data', function(done) {
     freshbooks.me(function(err, me) {
       if (err) return done(err);
-      console.log(JSON.stringify(me));
+
+      chai.assert.ok(me.id);
       done();
     });
   });
@@ -27,18 +52,11 @@ describe('FreshBooks Client', function() {
       var biz_id = me.business_ids[0];
       freshbooks.getProjects(biz_id, function(err, projects) {
         if (err) return done(err);
+
+        chai.assert.ok(projects.length);
+        chai.assert.ok(projects[0].id);
         done();
       });
     });
   });
-
-  // it('should connect & authorize successfully', function(done) {
-  //   var client_id = process.env.FBOOKS_CLIENT_ID;
-  //   var client_secret = process.env.FBOOKS_CLIENT_SECRET;
-  //   var code = process.env.FBOOKS_CODE;
-  //   if (!client_id || !client_secret || !code)
-  //     throw new Error('Required environment variables FBOOKS_CLIENT_ID, FBOOKS_CLIENT_SECRET, FBOOKS_CODE were not populated');
-
-  //   freshbooks.getTokens(client_id, client_secret, code, done);
-  // });
 });
